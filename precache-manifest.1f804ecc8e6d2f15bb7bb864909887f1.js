a0379e5acd2db4ae40d0ae250c82071014ce2a48
sandipsaha597 <40121135+sandipsaha597@users.noreply.github.com>
2020-05-16 20:43:52 +0530

    Add files via upload

---
+++ precache-manifest.1f804ecc8e6d2f15bb7bb864909887f1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49e1ceaa7adb9bdd037b811e4d22b699",
+    "url": "/edunomics/index.html"
+  },
+  {
+    "revision": "b6f2ce45cf20bc401e6f",
+    "url": "/edunomics/static/css/main.af4f013a.chunk.css"
+  },
+  {
+    "revision": "f86b46a22cf7e329dcfb",
+    "url": "/edunomics/static/js/2.15d756e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/edunomics/static/js/2.15d756e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6f2ce45cf20bc401e6f",
+    "url": "/edunomics/static/js/main.d6626b29.chunk.js"
+  },
+  {
+    "revision": "2974b0eb8268b558501b",
+    "url": "/edunomics/static/js/runtime-main.b45697df.js"
+  },
+  {
+    "revision": "a1861ba5a063ab512426b04833f8211b",
+    "url": "/edunomics/static/media/icon1.a1861ba5.svg"
+  },
+  {
+    "revision": "572c14d7f119192cf23ec2f22dada09a",
+    "url": "/edunomics/static/media/icon2.572c14d7.svg"
+  },
+  {
+    "revision": "8263cfbcf6efe2fb35689b1e7acac749",
+    "url": "/edunomics/static/media/icon3.8263cfbc.svg"
+  },
+  {
+    "revision": "7f04139555b7e1aa6a8d71936994d495",
+    "url": "/edunomics/static/media/intro-illustration_qneuer.7f041395.svg"
+  },
+  {
+    "revision": "fb1798c6aed1433042db4cfe1678ee87",
+    "url": "/edunomics/static/media/logo.fb1798c6.svg"
+  }
+]);
